fed04566b1a765daeb11944847bf8143bb8cade2
pedrohba1 <pedro.almeida@ufu.br>
2018-05-25 15:27:05 -0300

    comecei a questão 3, pulei a função de somar a PA

--- trabalho.pl
+++ trabalho.pl
@@ -2,20 +2,35 @@
 % If you want to create a file, visit that file with C-x C-f,
 % then enter the text in that file's own buffer.
 
-%n-ésimo termo de um PA
 
-
-nelemento(H, [H|_], 1):- !.
+nelemento(H, [H|_], 1).
 nelemento(X, [_|T],  K):-
 	K >1,
 	K1 is K-1,
 	nelemento(X,T,K1).
 
+%n-ésimo elemento de uma PA feito usando recursão em cauda:
 pa(1,Elem,_,Elem).
 pa(N,First,Ratio, Elem):-
 	N  >1,
 	N1 is N-1,
 	Next is First+Ratio,
 	pa(N1,Next,Ratio,Elem).
+
+%Soma dos termos da PA feito usando recursao em cauda:
+
+
+
+
+%itens da questao 3:
+%1.R: X = a.
+%2.R: X	= [].
+%3.R: False.
+%4.R: X = [a].
+%5.R: False.
+
+
+
+
 
 
