08bbd84b91858295e9491ec0e7e4d8f152d17f48
pedrohba1 <pedro.almeida@ufu.br>
2018-06-16 00:58:06 -0300

    coloquei a aula de hoje aí

--- estudo em arquivos/copiar arquivos/arquivo1.pl
+++ estudo em arquivos/copiar arquivos/arquivo1.pl
@@ -1,2 +1,1 @@
-cao(pitbull).
-sexo(masculino).
+ca3
---
+++ operador.pl
@@ -0,0 +1,22 @@
+% This buffer is for notes you don't want to save.
+% If you want to create a file, visit that file with C-x C-f,
+% then enter the text in that file's own buffe
+
+
+:-op(50,xfx,mae_de).
+
+
+:-op(50,fx,viado).
+
+
+:-op(50, xfx, multiplica).
+:-op(60,yfx,soma).
+
+X soma Y :-
+	X +
+      Y .
+
+
+viado matheus.
+
+mae_de(carla,pedro).
