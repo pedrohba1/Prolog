c2e10143ddaa40032c42a25bc3d5cd05d9c17273
pedrohba1 <pedro.almeida@ufu.br>
2018-06-21 16:38:43 -0300

    tem que fazer o menu lá ainda

--- estudo em arquivos/dados.pl
+++ estudo em arquivos/dados.pl
@@ -1,6 +1,21 @@
-:- dynamic pai/2.
 
-pai(pedro, maria).
-pai(pedro, joao).
-pai(pedro, joaquim).
+pai(pedro,maria).
+pai(pedro,joaquim).
+pai(pedro,ana).
+
+
+menu:-
+	write('1-listar n relacionamentos pai'),nl,
+	write('2-adicionar relacionamento pai'),nl,
+	write('3-remove relacionamento pai'),nl,
+	write('4-sair'),nl,
+	read(X),
+	opcao(X).
+
+
+
+
+opcao(1):-
+	repeat, pai(X,Y),
+	write('pai('), write(X), write(','), write(Y), write(')'),nl,menu,fail.
 
