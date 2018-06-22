16f658e9c70d8a48481b45d2b439cdf40cfdf212
pedrohba1 <pedro.almeida@ufu.br>
2018-06-22 14:48:56 -0300

    códigos de prolog

---
+++ estudo em arquivos/menu.pl
@@ -0,0 +1,58 @@
+
+:- dynamic pai/2.
+
+leia(X):-
+	read(X),
+
+
+
+
+menu:-
+	repeat,
+	consult('/home/pedro/Documentos/Prolog/estudo em arquivos/dados.pl'),
+	write('1-listar n relacionamentos pai'),nl,
+	write('2-adicionar relacionamento pai'),nl,
+	write('3-remove relacionamento pai'),nl,
+	write('4-sair'),nl,
+	read(X),
+	(X=4, write('fechando'), nl;
+	opcao(X),fail).
+
+opcao(1):-!,
+	listing(pai/2).
+opcao(2):-!,
+	write('escreva o nome do pai'),nl,
+	read(X),
+	write('escreva o nome do filho'),nl,
+	read(Y),
+	assertz(pai(X,Y)),
+	tell('/home/pedro/Documentos/Prolog/estudo em arquivos/dados.pl'),
+	listing(pai/2),
+	told.
+
+
+opcao(3):-!,
+	nl,
+	write('escreve o pai que quer remover'),nl,
+	read(X),
+	write('escrev1a o filho que quer remover'),nl,
+	read(Y),
+	retract(pai(X,Y)),
+	tell('/home/pedro/Documentos/Prolog/estudo em arquivos/dados.pl'),
+	listing(pai/2),
+	told.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
